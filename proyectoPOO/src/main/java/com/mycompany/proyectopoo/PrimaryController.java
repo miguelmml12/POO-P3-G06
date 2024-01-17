5b15bea0ddabb7b798790f55b51f56f2e3cd8ba0
Josue.jibaja <Josue.jibaja@DESKTOP-S1R5IBQ>
2024-01-17 17:41:38 -0500

    menuferia

---
+++ proyectoPOO/src/main/java/com/mycompany/proyectopoo/PrimaryController.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.proyectopoo;
+import modelo.*;
+import interfaces.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+/**
+ * FXML Controller class
+ *
+ * @author Josue.jibaja
+ * 
+ */
+
+public class PrimaryController implements Initializable {
+
+    /*private TableView<Feria> table;
+    private TableColumn<Feria,Integer> cod;
+    private TableColumn<Feria,String> name;
+    private TableColumn<Feria,String> f_inicio;
+    private TableColumn<Feria,String> place;
+    //private TableColumn<Feria,Integer> n_aus;
+    //private TableColumn<Feria,Integer> n_emp;
+    
+   /*  private TableView<ArrayList<Feria>> table;
+    private TableColumn<ArrayList<Feria>,String> cod;
+    private TableColumn<ArrayList<Feria>,String> name;
+    private TableColumn<ArrayList<Feria>,String> f_inicio;
+    private TableColumn<ArrayList<Feria>,String> place;
+    private TableColumn<ArrayList<Feria>,Integer> n_aus;
+    private TableColumn<ArrayList<Feria>,Integer> n_emp;
+
+    
+    ObservableList<ArrayList<Feria>> ferias = table.getItems();
+    ArrayList<Feria> fer = MenuFeria.ferias;
+
+     /* Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+     /*   cod.setCellValueFactory(new PropertyValueFactory<Feria,Integer>("codigo"));
+        name.setCellValueFactory(new PropertyValueFactory<Feria,String>("nombre"));
+        f_inicio.setCellValueFactory(new PropertyValueFactory<Feria,String>("descripcion"));
+        place.setCellValueFactory(new PropertyValueFactory<Feria,String>("lugar"));
+        //n_aus.setCellValueFactory(new PropertyValueFactory<Feria,Integer>("codigo"));
+        // ferias.add(fer);
+*/
+    }    
+    
+}
