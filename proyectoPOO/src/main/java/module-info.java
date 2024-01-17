1a284bf2af592571916239215b3155b1a68646f2
Josue.jibaja <Josue.jibaja@DESKTOP-S1R5IBQ>
2024-01-17 16:55:11 -0500

    menuferia

--- proyectoPOO/src/main/java/module-info.java
+++ proyectoPOO/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.proyectopoo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.proyectopoo to javafx.fxml;
     exports com.mycompany.proyectopoo;
