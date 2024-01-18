14b0ee2a4ff2f6877c2466a50c348988f276294b
Josue.jibaja <Josue.jibaja@DESKTOP-S1R5IBQ>
2024-01-17 23:04:27 -0500

    menuferias

--- proyectoPOO/src/main/java/com/mycompany/proyectopoo/MenuPrincipalController.java
+++ proyectoPOO/src/main/java/com/mycompany/proyectopoo/MenuPrincipalController.java
@@ -10,6 +10,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import java.io.IOException;
+import modelo.*;
+import interfaces.*;
+import interfaces.MenuFeria;
+// import interfaces.MenuEmprendedor;
+import java.util.Arrays;
 
 
 import javafx.scene.control.Button;
@@ -36,12 +41,34 @@ public class MenuPrincipalController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Thread t = new Thread( () ->
+        {
+             MenuFeria.agregarFeriaTest();
+    MenuEmprendedor.agregar4Emprendedores();
+    AuspicianteManager.agregarAspicianteInicial();
+     System.out.println(MenuFeria.ferias);
+
+        System.out.println("El codigo de la feria es el numero entero de la posicion en que fue creada (la feria ya creada (que se pide) tiene codigo 1)");
+        });
+        t.setDaemon(true);
+        t.start();
+   
+    
     }    
     
     @FXML
     private void abrirFer(ActionEvent event)throws IOException {
+//        Thread t = new Thread( () ->
+//        {
+//             MenuFeria.administracionDeFerias();
+//
+//        System.out.println("El codigo de la feria es el numero entero de la posicion en que fue creada (la feria ya creada (que se pide) tiene codigo 1)");
+//        });
+//        t.setDaemon(true);
+//        t.start();
             App.setRoot("primary");
+
+            
     }
 
         
