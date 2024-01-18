1b70ce0eb15b89bd4e492f04d7e4e2448a3491df
LENOVO <LENOVO@DESKTOP-210NLKD>
2024-01-18 01:30:24 -0500

    correccion menuaus

---
+++ proyectoPOO/src/main/java/com/mycompany/proyectopoo/MenuAuspicianteController.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.proyectopoo;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+/**
+ * FXML Controller class
+ *
+ * @author LENOVO
+ */
+public class MenuAuspicianteController implements Initializable {
+
+
+    @FXML
+    private Button nuevoAus;
+    @FXML
+    private Button editarAus;
+    @FXML
+    private Button salirAus;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void editarAus(ActionEvent event) {
+    }
+
+    @FXML
+    private void salirAus(ActionEvent event) {
+    }
+
+}
