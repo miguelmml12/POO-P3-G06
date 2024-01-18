c957030fe0348b1acf68d363fb3b2b8ea7506e19
Josue.jibaja <Josue.jibaja@DESKTOP-S1R5IBQ>
2024-01-18 02:52:09 -0500

    agregar Ferias :)

--- proyectoPOO/src/main/java/com/mycompany/proyectopoo/MenuFeriasController.java
+++ proyectoPOO/src/main/java/com/mycompany/proyectopoo/MenuFeriasController.java
@@ -5,8 +5,10 @@
 package com.mycompany.proyectopoo;
 
 import com.mycompany.proyectopoo.App;
+import interfaces.MenuFeria;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,6 +16,8 @@ import javafx.fxml.Initializable;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import modelo.Feria;
+import modelo.Stand;
 /**
  * FXML Controller class
  *
@@ -56,6 +60,52 @@ public class MenuFeriasController implements Initializable {
     
     @FXML
     private void guardar(ActionEvent event) {
+        
+        String nombre = name.getText();
+        String descripcion = description.getText();
+        String lugar = place.getText();
+        String fechaInicio = f_i.getText();
+        String fechaFin = f_f.getText();
+        String horario = time.getText();
+        
+        
+        ArrayList<Stand> seccion1 = new ArrayList<Stand>();
+        ArrayList<Stand> seccion2 = new ArrayList<Stand>();
+        ArrayList<Stand> seccion3 = new ArrayList<Stand>();
+        ArrayList<Stand> seccion4 = new ArrayList<Stand>();
+        
+        int b = 1;
+        int cantidadStands = Integer.parseInt(s_1.getText());
+            for (int i = 0; i < cantidadStands; i++) {
+                seccion1.add(new Stand("A" + b));
+              b++;
+            }
+            b = 0; //reseteo contador
+            cantidadStands = Integer.parseInt(s_2.getText());
+
+            for (int i = 0; i < cantidadStands; i++) {
+              b++;
+                seccion2.add(new Stand("B"+ b));
+            }
+          b = 0;
+          cantidadStands = Integer.parseInt(s_3.getText());
+
+
+            for (int i = 0; i < cantidadStands; i++) {
+              b++;
+                seccion3.add(new Stand("C"+ b));
+            }
+          b = 0;
+          cantidadStands = Integer.parseInt(s_4.getText());
+
+            for (int i = 0; i < cantidadStands; i++) {
+              b++;
+                seccion4.add(new Stand("D"+ b));
+            }
+
+            Feria nuevaFeria = new Feria(nombre, descripcion, lugar, fechaInicio, fechaFin, horario, seccion1, seccion2, seccion3, seccion4);
+            MenuFeria.ferias.add(nuevaFeria);
+        
     }
 
     @FXML
