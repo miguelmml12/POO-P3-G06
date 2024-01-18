10ad93bac881aee2c40cbec57c518a94dd9e6096
Josue.jibaja <Josue.jibaja@DESKTOP-S1R5IBQ>
2024-01-18 00:31:08 -0500

    menuStand_buttons

--- proyectoPOO/src/main/java/com/mycompany/proyectopoo/MenuStandsController.java
+++ proyectoPOO/src/main/java/com/mycompany/proyectopoo/MenuStandsController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import interfaces.*;
 import static interfaces.StandManager.menuStand;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import modelo.Feria;
@@ -50,26 +51,32 @@ public class MenuStandsController implements Initializable {
         if(MenuFeria.buscarFeriaPorCodigo(codFer) != null){
           Feria fer = MenuFeria.buscarFeriaPorCodigo(codFer);
           HBox frame1 = new HBox();
+          frame1.setAlignment(Pos.CENTER);
           for (Stand stand : fer.seccion1) {
               Label name = new Label(stand.getCodigo());
               frame1.getChildren().add(name);
         }
           HBox frame2 = new HBox();
+          frame2.setAlignment(Pos.CENTER);
         for (Stand stand : fer.seccion2) {
             Label name = new Label(stand.getCodigo());
               frame2.getChildren().add(name);
         }
         HBox frame3 = new HBox();
+        frame3.setAlignment(Pos.CENTER);
         for (Stand stand : fer.seccion3) {
             Label name = new Label(stand.getCodigo());
               frame3.getChildren().add(name);
         }
         HBox frame4 = new HBox();
+        frame4.setAlignment(Pos.CENTER);
         for (Stand stand : fer.seccion4) {
             Label name = new Label(stand.getCodigo());
               frame4.getChildren().add(name);
         }
-        tablaStands.getChildren().addAll(frame1,frame2,frame3,frame4);
+        Button bt = new Button("Asignar Stand");
+        Button btn = new Button("Consultar Stand");
+        tablaStands.getChildren().addAll(frame1,frame2,frame3,frame4,bt,btn);
       } 
        else {
 //        System.out.println("Feria no encontrada");
